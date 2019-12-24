ef6307afdbd1fb80d8ba5cc6b402070aad3aacb7
De Bene <benfelip@38f9d3c74477.ant.amazon.com>
2019-12-23 18:16:26 -0600

    Added bootstrap layout

--- src/App.js
+++ src/App.js
@@ -5,29 +5,65 @@ import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react'; 
 
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavLink from 'react-bootstrap/NavLink';
+
 Amplify.configure(awsconfig);
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header />
       </div>
     );
   }
 
 }
 export default withAuthenticator(App, true);
+
+class Header extends Component{
+  render(){
+    return (      
+        <BrowserRouter>
+          <div> 
+
+<Navbar bg="dark" variant="dark">
+  <Navbar.Brand href="#home">Oktank</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="#">MS SQL Load</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar>
+                        
+            <Switch>            
+              <Route exact path="/" component={Home} />             
+              <Route path="/*" component={NotFound} />                       
+            </Switch>   
+          </div>  
+        </BrowserRouter>              
+    )
+  }
+}
+
+class NotFound extends Component {
+  render(){
+    return <div>Not Found</div>
+  }
+}
+
+class Home extends Component {
+  render(){
+    return (
+      <div>
+        Home        
+      </div>
+    )
+  }
+}
