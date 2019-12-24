377de887056d64cb549607dd7f45f7bb27b61721
De Bene <benfelip@38f9d3c74477.ant.amazon.com>
2019-12-24 15:59:41 -0600

    Changed style, fix api and added text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
@@ -10,6 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavLink from "react-bootstrap/NavLink";
+import sqlFetcher from "./sqlFetcher";
 
 Amplify.configure(awsconfig);
 
@@ -37,8 +36,8 @@ class Header extends Component {
                 <Nav.Link as={Link} to="/">
                   Home
                 </Nav.Link>
-                <Nav.Link as={Link} to="#">
-                  MS SQL Load
+                <Nav.Link as={Link} to="/getSQL">
+                  Data Load
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
@@ -46,6 +45,7 @@ class Header extends Component {
 
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/getSQL" component={sqlFetcher} />
             <Route path="/*" component={NotFound} />
           </Switch>
         </div>
@@ -62,6 +62,21 @@ class NotFound extends Component {
 
 class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return ( <div><h1>Vehicle Routing Problem</h1>
+    <p> This a Proof of Concept (POC) frontend to allow users to load data from the legacy system (MS SQL 2012 Based). 
+    The idea is to add Machine Learning capabilities to an old application in a complete serverless way.
+    </p>
+    <h3>Problem statement</h3>
+      <img src="/imgs/Model.png" class="img-fluid rounded mx-auto d-block" alt="Graphical Problem Definition" />
+    
+    <p>Based on that list of vehicles that have to be delivered, this data is fed into <abbr title="S3 Bucket"> sm-input-bucket</abbr> where all the process start.
+      Having that data in that bucket, <abbr title="lambda function">runModel.py</abbr> function is triggered. This function has two main objectives :
+      Update the state table on <abbr title="Key-value pair database">Dynamo DB</abbr> with vehicles involved in the Sage Maker Job and trigger it.
+      Send that data to a S3 Bucket to allow a Amazon Sage Maker to calculate the best delivery route using Machine Learning.
+      Results can be seen here.</p>
+      <p>Once Machine Learning has finished running, the user can see a rendered image with the best-rewarded route.</p>
+  <p>This can be best depicted in this diagram : 
+      <img src="/imgs/arquitecture.png" class="img-fluid rounded mx-auto d-block" alt="architecture" />
+  </p></div>);
   }
 }
---
+++ src/sqlFetcher.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Amplify, { API } from "aws-amplify";
+import { Loading } from "aws-amplify-react";
+import Button from 'react-bootstrap/Button';
+
+class sqlFetcher extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      response: ""
+    };
+    this.getSQLData();
+  }
+
+  getSQLData = async () => {
+    const response = await API.get("getCarsA", "/getvh");
+    this.setState({ isLoading: true, response: response.data.message });
+    alert(JSON.stringify(response, null, 2));
+  };
+
+  render() {
+    const message = this.state.response;
+
+    return (
+      <div>
+        <h3>Let's load some data !</h3>
+        <p>
+          The following button will do a query on MS SQL Server in the 
+          <abbr>civar_ford</abbr> Database into our Bucket for later processing.<br />
+          <Button
+      variant="warning"
+      disabled={this.state.isLoading}
+      onClick={!this.state.isLoading ? this.getSQLData : null}
+    >
+      {this.state.isLoading ? 'Loading…' : 'Click to load'}
+    </Button>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default sqlFetcher;
