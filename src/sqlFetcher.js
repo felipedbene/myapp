17931a348f211f2c5d7c560bb67355ab29d6428b
De Bene <benfelip@38f9d3c74477.ant.amazon.com>
2019-12-24 19:08:32 -0600

    Fully working

--- src/sqlFetcher.js
+++ src/sqlFetcher.js
@@ -2,21 +2,26 @@ import React, { Component } from "react";
 import Amplify, { API } from "aws-amplify";
 import { Loading } from "aws-amplify-react";
 import Button from 'react-bootstrap/Button';
+import { Alert } from "react-bootstrap";
 
 class sqlFetcher extends Component {
   constructor() {
     super();
     this.state = {
       isLoading: false,
-      response: ""
+      calledWithSuccess : false,
+      response: ''
     };
     this.getSQLData();
   }
 
-  getSQLData = async () => {
+  getSQLData = async (res) => {
+    console.log("called method");
+    this.setState({ isLoading: true});
     const response = await API.get("getCarsA", "/getvh");
-    this.setState({ isLoading: true, response: response.data.message });
-    alert(JSON.stringify(response, null, 2));
+    this.setState({isLoading:false, response: response.message, calledWithSuccess : true});
+    console.log(this.state);
+
   };
 
   render() {
@@ -35,8 +40,8 @@ class sqlFetcher extends Component {
       disabled={this.state.isLoading}
       onClick={!this.state.isLoading ? this.getSQLData : null}
      >
-      {this.state.isLoading ? 'Loading…' : 'Click to load'}
-    </Button>
+      {this.state.isLoading ? ('Loading…') : ('Click to reload')}
+    </Button>{this.state.calledWithSuccess ? <Alert variant="success">{this.state.response}</Alert> : ''}
         </p>
       </div>
     );
