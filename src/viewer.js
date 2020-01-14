c3257008324e042778ff46729019ce912a833570
De Bene <benfelip@38f9d3c74477.ant.amazon.com>
2020-01-13 23:37:27 -0600

    Formating fix

--- src/viewer.js
+++ src/viewer.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { Storage } from "aws-amplify";
 import ReactLoading from "react-loading";
 import ReactPlayer from "react-player";
-import Card from 'react-bootstrap/Card';
-import CardColumns from 'react-bootstrap/CardColumns'
+import Card from "react-bootstrap/Card";
+import CardColumns from "react-bootstrap/CardColumns";
 import "./movie";
 
 class viewer extends Component {
@@ -18,8 +18,8 @@ class viewer extends Component {
   getFileList() {
     Storage.list("videos/", { level: "public" })
       .then(result => {
-        console.log(result)
-        const filtrado = result.filter( result => result.key.endsWith(".m3u8") )
+        console.log(result);
+        const filtrado = result.filter(result => result.key.endsWith(".m3u8"));
         filtrado.shift();
         filtrado.sort();
         filtrado.reverse();
@@ -35,35 +35,29 @@ class viewer extends Component {
     console.log(this.state.data);
 
     const fileList = this.state.data.map(archivo => (
-    <Card className="text-center" >
-          <Card.Body>
-    <Card.Title>{archivo.key.replace("videos/","")}</Card.Title>
-            
-      <ReactPlayer 
-      key={archivo.key} 
-      url={"http://d1qbcdoydtv3t1.cloudfront.net/" + archivo.key}
-      width='100%'
-      height='100%'
-      controls 
-      />
-       
-            </Card.Body>
-            </Card>
+      <Card className="text-center">
+        <Card.Body>
+          <Card.Title>{archivo.key.replace("videos/", "")}</Card.Title>
 
+          <ReactPlayer
+            key={archivo.key}
+            url={"http://d1qbcdoydtv3t1.cloudfront.net/" + archivo.key}
+            width="100%"
+            height="100%"
+            controls
+          />
+        </Card.Body>
+      </Card>
     ));
 
-
-  
     //const fileList = <ReactPlayer url="https://d1qbcdoydtv3t1.cloudfront.net/videos/FELIPO.m3u8" controls />
     return (
       <div>
         {this.state.isLoading ? (
           <ReactLoading type="spin" color="#FFA500" />
         ) : (
-          <CardColumns>{fileList}
-           
-</CardColumns>     
-          )}
+          <CardColumns>{fileList}</CardColumns>
+        )}
       </div>
     );
   }
